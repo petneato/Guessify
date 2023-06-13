a9d0eb8e29fbb1a15170cba55fde82485522cc52
petneato <alecmontesano@gmail.com>
2023-06-12 22:14:59 -0400

    Npm server is now working and things are structure correctly for MVC.
    I think I want to create a view.js object in controller
    and run a method from that view element that runs the NPM start.
    
    That way we can have methods make changes to the view at the request
    of the controller.

--- Client/Controller/control.js
+++
@@ -1,5 +0,0 @@
-import View from 'file:///C:/Users/Alec%20Montesano/Desktop/Guessify/Client/View/View.js';
-
-let viewInstance = new View("test", "test", "test", "test");
-
-View.start();
---
+++ Client/src/View/View.js
@@ -0,0 +1,21 @@
+import logo from './Spotify.png';
+import './View.css';
+
+function View() {
+  return (
+    <div className="Super">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Guessify</h1>
+        <h3>Spotify Guessing Game</h3>
+
+        <button>Log In</button>
+      </header>
+      <body>
+
+      </body>
+    </div>
+  );
+} 
+
+export default View;
