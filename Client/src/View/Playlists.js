7442feb0ad73199dd64562f4b12847351a3ead0e
petneato <alecmontesano@gmail.com>
2023-06-20 12:10:25 -0400

    Button functionality

---
+++ Client/src/View/Playlists.js
@@ -0,0 +1,16 @@
+function Playlists() {
+    return (
+        <div className="Super">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>Guessify</h1>
+            <h3>Spotify Guessing Game</h3>
+    
+            <button onClick={login}>Log In</button>
+          </header>
+          <body>
+    
+          </body>
+        </div>
+      );
+}
--- Client/src/View/View.js
+++ Client/src/View/View.js
@@ -1,5 +1,57 @@
 import logo from './Spotify.png';
 import './View.css';
+import { useHistory } from 'react-router-dom';
+
+
+const id = '0b0bea5cfeed47ee881242f0a154bbc9';
+const secretid = '3ca63b7d0b074a629b52a58a0ff9a7d4';
+
+var client_id = 'd9f97736297e4a039202cb31e162c0ef';
+var redirect_uri = 'http://localhost:3000/callback';
+
+const APIController = (function() {
+
+  const clientId = '';
+  const clientSecret = '';
+
+  const _getToken = async () => {
+    
+    const result = await fetch('http://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization' : 'Basic' + btoa(clientId + ':' + clientSecret)
+      },
+      body: 'grant_type=client_cridentials'
+    });
+
+    const data = await result.json();
+    return data.access_token;
+
+  }
+})();
+
+function login(){
+    const history = useHistory();
+
+    var state = 'test';
+    var stateKey = 'test';
+
+    localStorage.setItem(stateKey, state);
+    var scope = 'user-read-private user-read-email';
+
+    var url = 'https://accounts.spotify.com/authorize';
+    url += '?response_type=token';
+    url += '&client_id=' + encodeURIComponent(client_id);
+    url += '&scope=' + encodeURIComponent(scope);
+    url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+    url += '&state=' + encodeURIComponent(state);
+
+    console.log(url);
+    window.open(url);
+
+    history.push('/Paylist.js');
+}
 
 function View() {
   return (
@@ -9,7 +61,7 @@ function View() {
         <h1>Guessify</h1>
         <h3>Spotify Guessing Game</h3>
 
-        <button>Log In</button>
+        <button onClick={login}>Log In</button>
       </header>
       <body>
 
