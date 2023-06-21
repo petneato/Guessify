5c025639a49353d2956b37b74c7618a8df855ef6
Quinn <quinninthetardis@gmail.com>
2023-06-21 13:39:38 -0400

    moved to spotify.js and introduced getPlaylist()

---
+++ Client/src/View/Spotify.js
@@ -0,0 +1,108 @@
+
+var client_id = 'd9f97736297e4a039202cb31e162c0ef';
+var redirect_uri = 'http://localhost:3000/callback';
+
+async function generateCodeChallenge(codeVerifier) {
+    function base64encode(string) {
+        return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
+            .replace(/\+/g, '-')
+            .replace(/\//g, '_')
+            .replace(/=+$/, '');
+    }
+
+    const encoder = new TextEncoder();
+    const data = encoder.encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+
+    return base64encode(digest);
+}
+
+function generateRandomString(length) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+export function loginPKCE() {
+    let codeVerifier = generateRandomString(128);
+    window.localStorage.setItem("codeVerifier", codeVerifier);
+    generateCodeChallenge(codeVerifier).then(codeChallenge => {
+        let state = generateRandomString(16);
+        let scope = 'user-read-private user-read-email';
+
+        window.localStorage.setItem('code_verifier', codeVerifier);
+
+        let args = new URLSearchParams({
+            response_type: 'code',
+            client_id: client_id,
+            scope: scope,
+            redirect_uri: redirect_uri,
+            state: state,
+            code_challenge_method: 'S256',
+            code_challenge: codeChallenge
+        });
+
+        window.location = 'https://accounts.spotify.com/authorize?' + args;
+    });
+}
+
+export function getLoginToken(code, codeVerifier) {
+
+    let body = new URLSearchParams({
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: redirect_uri,
+        client_id: client_id,
+        code_verifier: codeVerifier
+    });
+
+    const response = fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: body
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('HTTP status ' + response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            window.localStorage.setItem('access_token', data.access_token);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
+}
+
+export async function getProfile(accessToken) {
+
+    const response = await fetch('https://api.spotify.com/v1/me', {
+        headers: {
+            Authorization: 'Bearer ' + accessToken
+        }
+    });
+
+    const data = await response.json();
+    return data;
+}
+
+export async function getPlaylist(accessToken, playlistURI){
+
+    const response = await fetch('https://api.spotify.com/v1/playlists/'+playlistURI, {
+        headers: {
+            Authorization: 'Bearer ' + accessToken
+        }
+    });
+
+    let data = await response.json();
+    // console.log(data);
+    return data;
+}
