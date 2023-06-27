6c728c68cc50ead28fa78c927d00a35269aa5415
petneato <alecmontesano@gmail.com>
2023-06-27 14:45:53 -0400

    Framework for playlist.js layed out

--- Client/src/View/Playlists.js
+++ Client/src/View/Playlists.js
@@ -1,15 +1,18 @@
-function Playlists() {
+function Playlists(json) {
+  json.
     return (
         <div className="Super">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1>Guessify</h1>
-            <h3>Spotify Guessing Game</h3>
-    
-            <button onClick={login}>Log In</button>
+            <h1>Select a Playlist</h1>
           </header>
+
           <body>
-    
+            <form>
+              <div className='listCard'>
+                <img href='' alt='Playlist Cover Image'></img>
+                <h5 className='listName'></h5>
+              </div>
+            </form>
           </body>
         </div>
       );
