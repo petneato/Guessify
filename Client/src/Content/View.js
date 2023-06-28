768426534f1d33f51a11a11e2fd78a5e225e4f41
petneato <alecmontesano@gmail.com>
2023-06-28 12:51:10 -0400

    Adjusted paths for new structure and styled the playlist cards v1.0

--- Client/src/Content/View.js
+++ Client/src/Content/View.js
@@ -1,7 +1,7 @@
 import {loginPKCE, getProfile, getLoginToken, getPlaylist} from './Spotify.js'
 
 import logo from '../Images/Spotify.png';
-import 'C:\\Users\\alecm\\Desktop\\GuessifySProj\\Client\\src\\CSS\\View.css';
+import '../CSS/View.css'
 
 function View() {
 
